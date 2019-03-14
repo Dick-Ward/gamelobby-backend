c0595cff9b9ebec8e1ce4e757fcd27e559863532
gonzodamus <github email address>
2019-03-14 14:53:56 -0400

    actioncable implemented but firing twice

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,4 +5,17 @@ class MessagesController < ApplicationController
     render json: messages
   end
 
+  def create
+    message = Message.new(message_params)
+    if message.save
+      ActionCable.server.broadcast("message_channel", message)
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :chatroom_id, :user_id)
+  end
+
 end
